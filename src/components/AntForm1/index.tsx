5d258416feb59d6927aa42846d446ac6bef72276
ujike-ryota <ujikeryota@vivit.co.jp>
2021-10-11 04:46:33 +0900

    add antd component page

--- src/components/AntForm1/index.tsx
+++ src/components/AntForm1/index.tsx
@@ -1,38 +1,68 @@
 import * as Ant from "antd";
-import { useFormContext } from "react-hook-form";
+import { useFormContext, Controller } from "react-hook-form";
 import * as yup from "yup";
 
-export const Form1Validation = {
+export const AntForm1Validation = {
   name: yup.string().required(),
   gender: yup.string().required(),
   age: yup.number().positive().integer().required(),
 };
 
 export const AntForm1 = () => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
   return (
-    <Ant.Form>
-      <Ant.Form.Item label="名前">
-        <Ant.Input />
-      </Ant.Form.Item>
+    <>
+      <Controller
+        control={control}
+        name="name"
+        render={({ field, fieldState }) => (
+          <Ant.Form.Item
+            label="名前"
+            validateStatus={fieldState.error ? "error" : ""}
+            help={errors.name?.message}
+            required
+          >
+            <Ant.Input {...field} />
+          </Ant.Form.Item>
+        )}
+      />
 
-      <Ant.Form.Item label="性別">
-        <Ant.Radio.Group>
-          <Ant.Radio value="male">男</Ant.Radio>
-          <Ant.Radio value="female">女</Ant.Radio>
-          <Ant.Radio value="other">その他</Ant.Radio>
-        </Ant.Radio.Group>
-      </Ant.Form.Item>
+      <Controller
+        control={control}
+        name="gender"
+        render={({ field, fieldState }) => (
+          <Ant.Form.Item
+            label="性別"
+            validateStatus={fieldState.error ? "error" : ""}
+            help={errors.gender?.message}
+            required
+          >
+            <Ant.Radio.Group {...field}>
+              <Ant.Radio value="male">男</Ant.Radio>
+              <Ant.Radio value="female">女</Ant.Radio>
+              <Ant.Radio value="other">その他</Ant.Radio>
+            </Ant.Radio.Group>
+          </Ant.Form.Item>
+        )}
+      />
 
-      <Ant.Form.Item label="生年月日">
-        <Ant.DatePicker />
-      </Ant.Form.Item>
-
-      <Ant.Form.Item label="年齢">
-        <Ant.Input />
-      </Ant.Form.Item>
-      <Ant.Form.Item label="メールアドレス">
-        <Ant.Input />
-      </Ant.Form.Item>
-    </Ant.Form>
+      <Controller
+        control={control}
+        name="age"
+        render={({ field, fieldState }) => (
+          <Ant.Form.Item
+            label="年齢"
+            validateStatus={fieldState.error ? "error" : ""}
+            help={errors.age?.message}
+            required
+          >
+            <Ant.Input {...field} />
+          </Ant.Form.Item>
+        )}
+      />
+    </>
   );
 };
